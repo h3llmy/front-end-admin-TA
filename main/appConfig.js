187feab1693b32e8062ee8480a9afa9663ac6dc5
Helmi Trisna <helmytrisna86@gmail.com>
2023-07-05 19:42:13 +0800

    fix production config

--- main/appConfig.js
+++ main/appConfig.js
@@ -1,16 +1,17 @@
 // development config
-export default {
-  BASE_API_URL: "http://localhost:3000/api/v1",
-  COOKIE_PASSWORD:
-    "3e67b7bd812e8318a4346e8f02a03ff4806b8c455fe854a0cb7403082a110d6227caa11dc299b74980166937277949772283427d98f13014023007045f268892",
-  WEB_URL: "http://localhost:3001/reset-password",
-};
-
-// production config
 // export default {
-//   BASE_API_URL:
-//     "https://h3llmy-friendly-garbanzo-gjqqjpx95ww3p95j-3000.preview.app.github.dev/api/v1",
+//   BASE_API_URL: "http://localhost:3000/api/v1",
 //   COOKIE_PASSWORD:
 //     "3e67b7bd812e8318a4346e8f02a03ff4806b8c455fe854a0cb7403082a110d6227caa11dc299b74980166937277949772283427d98f13014023007045f268892",
-//   WEB_URL: "https://8e84-101-128-97-201.ngrok-free.app/reset-password",
+//   WEB_URL: "http://localhost:3001/reset-password",
 // };
+
+// production config
+export default {
+  BASE_API_URL:
+    "https://h3llmy-friendly-garbanzo-gjqqjpx95ww3p95j-3000.preview.app.github.dev/api/v1",
+  COOKIE_PASSWORD:
+    "3e67b7bd812e8318a4346e8f02a03ff4806b8c455fe854a0cb7403082a110d6227caa11dc299b74980166937277949772283427d98f13014023007045f268892",
+  WEB_URL:
+    "https://h3llmy-psychic-winner-p4rr467vp5ghr9pv-3001.preview.app.github.dev/reset-password",
+};
